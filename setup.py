90ff71811734f9dc4a14ff9c22c42076fca73ce8
bharti2810 <bhartiarora2810@gmail.com>
2022-02-16 16:12:11 +0530

    pytest , tox andsetup done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "src",
+    version = "0.0.1" ,# it is first version
+    description = "its a wine Q package",
+    author = "bharti2810",
+    packages = find_packages(),
+    license = "MIT"
+)
